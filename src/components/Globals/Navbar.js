9c5ca976f7cef9801e002f1c25128c35ce2660b5
Lerena Holloway <holloway.lerena@gmail.com>
2019-04-20 11:23:01 -0400

    Adds toggle bar

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
-import headshot from "../../images/headshot.jpg"
+// import headshot from "../../images/headshot.jpg"
+import portfolio from "../../images/portfolio.png"
+// import { FaCartArrowDown } from "react-icons/fa"
 
 export default class Navbar extends Component {
   state = {
@@ -26,18 +28,39 @@ export default class Navbar extends Component {
   }
   navbarHandler = () => {
     console.log("hello")
+    this.state.navbarOpen
+      ? this.setState({ navbarOpen: false, css: "collapse navbar-collapse" })
+      : this.setState({
+          navbarOpen: true,
+          css: "collapse navbar-collapse show",
+        })
   }
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-light navbar-light">
-        <Link to="/" className="navbar-brand" />
-        <img src={headshot} alt="logo" />
+      <nav className="navbar navbar-expand-lg bg-light navbar-light">
+        <Link to="/" className="navbar-brand">
+          <img src={portfolio} alt="logo" />
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
           onClick={this.navbarHandler}
-        />
-        <span className="navbar-toggler-icon" />
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={this.state.css}>
+          <ul className="navbar-nav mx-auto">
+            {this.state.links.map(link => {
+              return (
+                <li key={link.id} className="nav-item">
+                  <Link to={link.path} className="nav-link text-capitalize">
+                    {link.text}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </nav>
     )
   }
