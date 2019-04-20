34f07b58651eb5381c75c354988b06ac23448dca
Lerena Holloway <holloway.lerena@gmail.com>
2019-04-20 12:19:01 -0400

    adds footer section

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,11 +12,13 @@ import { StaticQuery, graphql } from "gatsby"
 import "./bootstrap.min.css"
 import "./layout.css"
 import Navbar from "./Globals/Navbar"
+import Footer from "./Globals/Footer"
 
 const Layout = ({ children }) => (
   <>
     <Navbar />
     {children}
+    <Footer />
   </>
 )
 
