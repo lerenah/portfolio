7b22e2492ca070a9083384c84228a1663549555d
Lerena Holloway <holloway.lerena@gmail.com>
2019-04-20 11:54:49 -0400

    adds hero image

---
+++ src/components/Globals/BackgroundImage.js
@@ -0,0 +1,18 @@
+import React from "react"
+import BackgroundImage from "gatsby-background-image"
+
+export default function Background({ img, styleClass, title, children }) {
+  return (
+    <BackgroundImage className={styleClass} fluid={img}>
+      <h1 className="title text-white text-center display-4 font-weight-bold">
+        {title}
+      </h1>
+      {children}
+    </BackgroundImage>
+  )
+}
+
+Background.defaultProps = {
+  title: "default title",
+  styleClass: "default-background",
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { FaGulp } from "react-icons/fa"
+import BackgroundImage from "../components/Globals/BackgroundImage"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <BackgroundImage
+      img={data.img.childImageSharp.fluid}
+      title="Hello World!"
+      styleClass="default-background"
+    />
     <FaGulp />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "headshot.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
