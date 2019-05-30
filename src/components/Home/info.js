eaf760630e1a51c4b8f6c3d2b103eb8323b8adee
Lerena Holloway <holloway.lerena@gmail.com>
2019-05-30 10:01:32 -0400

    changes info

--- src/components/Home/info.js
+++ src/components/Home/info.js
@@ -6,11 +6,11 @@ export default function info() {
   return (
     <section className="py-5">
       <div className="container">
-        <Title title="my story" />
+        <Title title="Heya!" />
         <div className="row">
           <div className="col-10 col-sm-8 mx-auto text-center">
             <p className="lead text-muted mb-5">
-            I am a software engineer . I am eager to work on projects that facilitate human connection + harness creative thinking—and ultimately make our world a brighter place.
+            I am a software engineer with an untouchable work ethic. Let's build a project that will address the world's top economic objectives, starting with ourselves.
             </p>
             <Link to="/about">
               <button className="btn text-uppercase btn-yellow">
